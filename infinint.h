1ef1d4e195477674fa6c1df3b82b1014a53d0e66
tgejesse <jrinaldi@udel.edu>
2016-03-02 03:56:05 +0000

    fixes

--- infinint.h
+++ infinint.h
@@ -7,12 +7,12 @@ using namespace std;
 
 #ifndef InfinInt_H
 #define InfinInt_H
-class InfinInt{
-    private:
-       vector<bitset<4> > vecBitSet;
-       bitset<4> bitify(char c);
-    public:
-        InfinInt(std::string number);
-        void asBits();
-};
+    class InfinInt{
+        private:
+           vector<bitset<4> > vecBitSet;
+           bitset<4> bitify(char c);
+        public:
+            InfinInt(string number);
+            void asBits();
+    };
 #endif
