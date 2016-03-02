2d25ede3820eed7dd3994d68825b90f14affa9cd
tgejesse <jrinaldi@udel.edu>
2016-03-02 03:59:52 +0000

    fixes

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main() {
-   InfinInt* i = new InfinInt("586");
-   i->asBits(); // Should output 0101 1000 0110
+   InfinInt* i = new InfinInt("3967994432"); // Larger than 4294967295  ;)
+   i->asBits(); // Should output 0011 1001 0110 0111 1001 1001 0100 0100 0011 0010
    delete i;
 }
